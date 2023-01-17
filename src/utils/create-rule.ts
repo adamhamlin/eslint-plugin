685ad049199d62008766064fb4a7d847692a69d1
Adam C Hamlin <achamlin@gmail.com>
2023-01-16 23:13:25 -0500

    fix: docs links

--- src/utils/create-rule.ts
+++ src/utils/create-rule.ts
@@ -1,6 +1,8 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
 
+import { version } from '../../package.json';
+
 // Create rule function
 export const createRule = ESLintUtils.RuleCreator(
-    (ruleName) => `https://github.com/adamhamlin/eslint-plugin/docs/rules/${ruleName}.md`
+    (ruleName) => `https://github.com/adamhamlin/eslint-plugin/blob/v${version}/docs/rules/${ruleName}.md`
 );
